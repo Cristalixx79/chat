f2605f42a13d936245a92ccf96adaa7b8d3fa79f
Cristalixx79 <deinekoneko06@gmail.com>
2025-05-17 23:02:39 +0300

    3.0 version classes added

--- client/client.cpp
+++ client/client.cpp
@@ -14,30 +14,39 @@ const std::vector<std::string> localCommandList({"/exit", "/help"});
 const std::vector<std::string> publicCommandList({"/msg"});
 }
 
-bool ValidateMessage(const std::string& message);
+class Client {
+private:
+    std::string username;
+    sockaddr_in server;
+    int client;
 
-void ReceiveMessage(int client) {
-    char buffer[kMaxMessageSize]{};
+    std::string WriteMessage() {
+        std::string message = ColorMessage();
+        if (ValidateMessage(message)) return message;
 
-    while (true) {
-        std::memset(buffer, 0, kMaxMessageSize);
-        ssize_t bytes_received = recv(client, buffer, sizeof(buffer), 0);
-
-        if (std::string(buffer) == "/ban") {
-            std::cout << "\033[91m -- You have been permanently banned!\033[0m\n";
-            break;
+        std::cout << "\033[91m -- Такой команды нет, попробуйте снова\033[0m\n";
+        bool isValidated = false;
+        while (!isValidated) {
+            message = ColorMessage();
+            if (ValidateMessage(message)) isValidated = true;
+            else std::cout << "\033[91m -- Такой команды нет, попробуйте снова\033[0m\n";
         }
-        if (bytes_received <= 0){
-            std::cout << "\033[91m -- Сервер отключился\033[0m\n";
-            break;
-        }
-        std::cout << "\033[90m" << buffer << "\033[0m\n";
+        return message;
     }
-    close(client);
-    std::exit(EXIT_FAILURE);
-}
+    std::string ValidateName() {
+        bool isValidated = false;
+        std::string name = "";
 
-std::string ColorMessage() {
+        while (!isValidated) {
+            name = ColorMessage();
+
+            if (name.find(" ") == std::string::npos) isValidated = true;
+            else std::cout << "\033[91m -- Имя должно состоять из одного слова\033[0m\n";
+        }
+
+        return name;
+    }
+    std::string ColorMessage() {
     std::string message = "";
     std::cout << "\033[90m";
     std::getline(std::cin, message);
@@ -45,77 +54,45 @@ std::string ColorMessage() {
 
     return message;
 }
-
-std::string WriteMessage() {
-    std::string message = ColorMessage();
-    if (ValidateMessage(message)) return message;
-
-    std::cout << "\033[91m -- Такой команды нет, попробуйте снова\033[0m\n";
-    bool isValidated = false;
-    while (!isValidated) {
-        message = ColorMessage();
-        if (ValidateMessage(message)) isValidated = true;
-        else std::cout << "\033[91m -- Такой команды нет, попробуйте снова\033[0m\n";
+    bool ValidateMessage(const std::string& message) {
+        if (message[0] == '/' && message.find(" ") != std::string::npos) {
+            if (message.find("-") == std::string::npos) return false;
+            std::string command = message.substr(0, message.find(" ")).substr(0, message.find("-"));
+            for (auto i : publicCommandList) {
+                if (i == command) return true;
+            }
+            return false;
+        } else if (message[0] == '/' && message.find(" ") == std::string::npos) {
+            for (auto i : localCommandList) {
+                if (i == message) return true;
+            }
+            return false;
+        } else return true;
     }
-    return message;
-}
-std::string ValidateName() {
-    bool isValidated = false;
-    std::string name = "";
-
-    while (!isValidated) {
-        name = ColorMessage();
-
-        if (name.find(" ") == std::string::npos) isValidated = true;
-        else std::cout << "\033[91m -- Имя должно состоять из одного слова\033[0m\n";
+    void PrintHelpMenu() {
+        std::cout << "\n -- Доступные команды -- \n/msg-\"имя пользователя\" - отправить личное сообщение\n/exit - выйти из программы\n\n";
     }
+    void ReceiveMessage(int client) {
+        char buffer[kMaxMessageSize]{};
 
-    return name;
-}
+        while (true) {
+            std::memset(buffer, 0, kMaxMessageSize);
+            ssize_t bytes_received = recv(client, buffer, sizeof(buffer), 0);
 
-bool ValidateMessage(const std::string& message) {
-    if (message[0] == '/' && message.find(" ") != std::string::npos) {
-        if (message.find("-") == std::string::npos) return false;
-        std::string command = message.substr(0, message.find(" ")).substr(0, message.find("-"));
-        for (auto i : publicCommandList) {
-            if (i == command) return true;
+            if (std::string(buffer) == "/ban") {
+                std::cout << "\033[91m -- You have been permanently banned!\033[0m\n";
+                break;
+            }
+            if (bytes_received <= 0){
+                std::cout << "\033[91m -- Сервер отключился\033[0m\n";
+                break;
+            }
+            std::cout << "\033[90m" << buffer << "\033[0m\n";
         }
-        return false;
-    } else if (message[0] == '/' && message.find(" ") == std::string::npos) {
-        for (auto i : localCommandList) {
-            if (i == message) return true;
-        }
-        return false;
-    } else return true;
-}
-
-void PrintHelpMenu() {
-    std::cout << "\n -- Доступные команды -- \n/msg-\"имя пользователя\" - отправить личное сообщение\n/exit - выйти из программы\n\n";
-}
-
-int main() {
-    std::system("clear");
-
-    int client = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (client == -1) {
-        std::cerr << "\033[91mНе удалось создать сокет\033[0m\n";
-        return 1;
-    }
-    sockaddr_in server{};
-
-    server.sin_addr.s_addr = inet_addr("192.168.2.54");
-    server.sin_family = AF_INET;
-    server.sin_port = htons(54000);
-
-    if (connect(client , (sockaddr *)&server , sizeof(server)) < 0) {
-        std::cerr << "\033[91m -- Не удалось подключиться к серверу\033[0m\n";
         close(client);
-        return 1;
+        std::exit(EXIT_FAILURE);
     }
-
-    std::string username = "";
-    {
+    void Registrate() {
         std::cout << "\033[92m -- Подключено к серверу!\n -- Ведите ваше имя: \033[0m";
         username = ValidateName();
 
@@ -133,20 +110,52 @@ int main() {
                 send(client, username.c_str(), username.size(), 0);
             }
         }
-        if(username == "/exit") return 0;
+        if(username == "/exit") std::exit(EXIT_SUCCESS);
+    }
+public:
+    Client() {
+        client = socket(AF_INET, SOCK_STREAM, 0);
+        if (client == -1) {
+            std::cerr << "\033[91mНе удалось создать сокет\033[0m\n";
+            std::exit(EXIT_FAILURE);
+        }
+        sockaddr_in server = {};
+        server.sin_addr.s_addr = inet_addr("192.168.0.88");
+        server.sin_family = AF_INET;
+        server.sin_port = htons(54000);
+
+        if (connect(client , (sockaddr *)&server , sizeof(server)) < 0) {
+            std::cerr << "\033[91m -- Не удалось подключиться к серверу\033[0m\n";
+            close(client);
+            std::exit(EXIT_FAILURE);
+        }
+        Registrate();
     }
 
-    std::cout << "\033[92m -- Добро пожаловать в чат, " << username << "!\033[0m\n\n";
-    std::thread(ReceiveMessage, client).detach();
+    void StartClient() {
+        std::cout << "\033[92m -- Добро пожаловать в чат, " << username << "!\033[0m\n\n";
+        std::thread th(&Client::ReceiveMessage, this, client);
+        th.detach();
 
-    while(true) {
-        std::string message = WriteMessage();
-        if (message == "/exit") break;
-        if (message == "/help") PrintHelpMenu();
-        else send(client, message.c_str(), message.size(), 0);
+        while(true) {
+            std::string message = WriteMessage();
+            if (message == "/exit") break;
+            if (message == "/help") PrintHelpMenu();
+            else send(client, message.c_str(), message.size(), 0);
+        }
     }
 
-    std::cout << "\n\033[93m -- Отключение...\033[0m\n";
-    close(client);
+    ~Client() {
+        std::cout << "\n\033[93m -- Отключение...\033[0m\n";
+        close(client);
+    }
+};
+
+int main() {
+    std::system("clear");
+
+    Client client;
+    client.StartClient();
+
     return 0;
 }
--- server/server.cpp
+++ server/server.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 #include <cstring>
@@ -26,211 +27,226 @@ const std::string ban = "/ban";
 const std::vector<std::string> adminCommands({"/warn", "/ban"});
 }
 
-std::vector<std::pair<int, std::string>> users;
-std::mutex clientMutex;
+class Server {
+private:
+    std::vector<std::pair<int, std::string>> users;
+    std::mutex clientMutex;
+    sockaddr_in address;
+    int serverSocket;
+    int counter;
 
-int counter = 0;
+    void AdminPanel() {
+        while (true) {
+            std::string action = "";
+            std::getline(std::cin, action);
+            std::string command = action.substr(0, action.find(" "));
+            std::string user = action.substr(action.find(" ") + 1, action.size() - 1);
+            if (action.find(" ") == std::string::npos) {
+                std::cout << "\033[91m -- Неправильный формат команды \033[0m\n";
+                continue;
+            }
 
-void SendBroadcastMessage(const std::string& message, int receiverSocket) {
-    std::lock_guard<std::mutex> lock(clientMutex);
-    for (auto it : users) {
-        if (it.first != receiverSocket) {
-            send(it.first, message.c_str(), message.size(), 0);
-        }
-    }
-}
-void SendPrivateMessage(const std::string& message, int receiverSocket, int clientSocket) {
-    std::lock_guard<std::mutex> lock(clientMutex);
-    for (auto it : users) {
-        if (it.first == receiverSocket) {
-            send(it.first, message.c_str(), message.size(), 0);
-            break;
-        }
-    }
-}
+            if (!IsCommandFound(command)) {
+                std::cout << "\033[91m -- Команда \"" << command << "\" не найдена. Попробуйте снова: \033[0m";
+                continue;
+            }
+            if (!IsUserFound(user)) {
+                std::cout << "\033[91m -- Пользователь \"" << user << "\" не найден. Попробуйте снова: \033[0m";
+                continue;
+            }
 
-void ClientHandle(int clientSocket) {
-    char username[kMaxUsernameSize]{};
-    bool isRegistrated = false;
-    ssize_t clientBytesReceived = 0;
-
-    while (!isRegistrated) {
-        memset(username, 0, sizeof(username));
-        ssize_t clientBytesReceived = recv(clientSocket, username, sizeof(username), 0);
-
-        if (users.size() == 0) {
-            isRegistrated = true;
-        } else {
-            for (size_t i = 0; i < users.size(); i++) {
-                if (std::string(username) == users[i].second) {
-                    isRegistrated = false;
-                    break;
-                } else {
-                    isRegistrated = true;
+            if (command == "/warn") {
+                std::lock_guard<std::mutex> lock(clientMutex);
+                for (auto it = users.begin(); it != users.end(); it++) {
+                    if ((*it).second == user) {
+                        send((*it).first, warning.c_str(), warning.size(), 0);
+                        break;
+                    }
+                }
+            } else if (command == "/ban") {
+                std::lock_guard<std::mutex> lock(clientMutex);
+                for (auto it = users.begin(); it != users.end(); it++) {
+                    if ((*it).second == user) {
+                        send((*it).first, ban.c_str(), ban.size(), 0);
+                        break;
+                    }
                 }
             }
         }
-
-        if (isRegistrated) {
-            send(clientSocket, registrationSuccsses.c_str(), registrationSuccsses.size(), 0);
-        } else {
-            send(clientSocket, registrationError.c_str(), registrationError.size(), 0);
-        }
     }
-    counter++;
-    std::cout << "\033[92m -- Пользователь " << username << " подключился! Всего пользователей: " << counter << "\033[0m\n";
+    void ClientHandle(int clientSocket) {
+        char username[kMaxUsernameSize]{};
+        bool isRegistrated = false;
+        ssize_t clientBytesReceived = 0;
 
-    {
-        std::lock_guard<std::mutex> lock(clientMutex);
-        users.push_back(std::pair(clientSocket, username));
-    }
+        while (!isRegistrated) {
+            memset(username, 0, sizeof(username));
+            ssize_t clientBytesReceived = recv(clientSocket, username, sizeof(username), 0);
 
-    char buffer[kMaxMessageSize]{};
-    while (true) {
-        memset(buffer, 0, sizeof(buffer));
-        ssize_t clientBytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+            if (users.size() == 0) {
+                isRegistrated = true;
+            } else {
+                for (size_t i = 0; i < users.size(); i++) {
+                    if (std::string(username) == users[i].second) {
+                        isRegistrated = false;
+                        break;
+                    } else {
+                        isRegistrated = true;
+                    }
+                }
+            }
 
-        if (clientBytesReceived <= 0) {
-            std::cout << "\033[91m -- Пользователь " << username << " отключился! Всего пользователей: " << (users.size() - 1) << "\033[0m\n";
-            counter--;
-            break;
+            if (isRegistrated) {
+                send(clientSocket, registrationSuccsses.c_str(), registrationSuccsses.size(), 0);
+            } else {
+                send(clientSocket, registrationError.c_str(), registrationError.size(), 0);
+            }
+        }
+        counter++;
+        std::cout << "\033[92m -- Пользователь " << username << " подключился! Всего пользователей: " << counter << "\033[0m\n";
+
+        {
+            std::lock_guard<std::mutex> lock(clientMutex);
+            users.push_back(std::pair<int, std::string>(clientSocket, username));
         }
 
-        std::string message(username, CountChar(username));
-        message.append(": ");
-        message.append(buffer);
-        message.shrink_to_fit();
+        char buffer[kMaxMessageSize]{};
+        while (true) {
+            memset(buffer, 0, sizeof(buffer));
+            ssize_t clientBytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
 
-        if (message.find("/msg-") != std::string::npos) {
-            std::string temp = message.erase(0, message.find("/msg-") + 5);
-            std::string address = temp.erase(temp.find(" "), std::string::npos);
-            bool isSent = false;
+            if (clientBytesReceived <= 0) {
+                std::cout << "\033[91m -- Пользователь " << username << " отключился! Всего пользователей: " << (users.size() - 1) << "\033[0m\n";
+                counter--;
+                break;
+            }
+
+            std::string message(username, CountChar(username));
+            message.append(": ");
+            message.append(buffer);
+            message.shrink_to_fit();
+
+            if (message.find("/msg-") != std::string::npos) {
+                std::string temp = message.erase(0, message.find("/msg-") + 5);
+                std::string address = temp.erase(temp.find(" "), std::string::npos);
+                bool isSent = false;
+                for (auto it = users.begin(); it != users.end(); it++) {
+                    if ((*it).second == address) {
+                        std::string privateMessage = std::string(username) + "(private):" + message.substr(message.find(" "), message.size());
+                        SendPrivateMessage(privateMessage, (*it).first, clientSocket);
+                        isSent = true;
+                        break;
+                    }
+                }
+                if (!isSent) send(clientSocket, privateMessageSendingError.c_str(), privateMessageSendingError.size(), 0);
+            } else {
+                std::cout << "\033[90m >> Отправлено пользователем " << message << "\033[0m\n";
+                SendBroadcastMessage(message, clientSocket);
+            }
+        }
+
+        close(clientSocket);
+        {
+            std::lock_guard<std::mutex> lock(clientMutex);
             for (auto it = users.begin(); it != users.end(); it++) {
-                if ((*it).second == address) {
-                    std::string privateMessage = std::string(username) + "(private):" + message.substr(message.find(" "), message.size());
-                    SendPrivateMessage(privateMessage, (*it).first, clientSocket);
-                    isSent = true;
+                if ((*it).first == clientSocket && (*it).second == username) {
+                    users.erase(it);
                     break;
                 }
             }
-            if (!isSent) send(clientSocket, privateMessageSendingError.c_str(), privateMessageSendingError.size(), 0);
-        } else {
-            std::cout << "\033[90m >> Отправлено пользователем " << message << "\033[0m\n";
-            SendBroadcastMessage(message, clientSocket);
         }
     }
-
-    close(clientSocket);
-    {
-        std::lock_guard<std::mutex> lock(clientMutex);
+    bool IsCommandFound(const std::string& command) {
+        for (auto i : adminCommands) {
+            if (command == i) return true;
+        }
+        return false;
+    }
+    bool IsUserFound(const std::string& user) {
         for (auto it = users.begin(); it != users.end(); it++) {
-            if ((*it).first == clientSocket && (*it).second == username) {
-                users.erase(it);
+            if ((*it).second == user) {
+                return true;
+            }
+        }
+        return false;
+    }
+    void SendBroadcastMessage(const std::string& message, int receiverSocket) {
+        std::lock_guard<std::mutex> lock(clientMutex);
+        for (auto it : users) {
+            if (it.first != receiverSocket) {
+                send(it.first, message.c_str(), message.size(), 0);
+            }
+        }
+    }
+    void SendPrivateMessage(const std::string& message, int receiverSocket, int clientSocket) {
+        std::lock_guard<std::mutex> lock(clientMutex);
+        for (auto it : users) {
+            if (it.first == receiverSocket) {
+                send(it.first, message.c_str(), message.size(), 0);
                 break;
             }
         }
     }
-}
 
-bool IsCommandFound(const std::string& command) {
-    for (auto i : adminCommands) {
-        if (command == i) return true;
+public:
+    Server() {
+        counter = 0;
+        serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+        if (serverSocket == -1) {
+            std::cerr << "\033[91m -- Ошибка создания сокета\033[0m\n";
+            std::exit(EXIT_FAILURE);
+        }
+
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons(54000);
+
+        if (bind(serverSocket, (sockaddr*)&address, sizeof(address)) < 0) {
+            std::cerr << "\033[91m -- Ошибка bind\033[0m\n";
+            close(serverSocket);
+            std::exit(EXIT_FAILURE);
+        }
+
+        if (listen(serverSocket, kMaxBacklog) < 0) {
+            std::cerr << "\033[91m -- Ошибка listen\033[0m\n";
+            close(serverSocket);
+            std::exit(EXIT_FAILURE);
+        }
+
+        std::cout << "\033[90m -- Ожидение пользователей...\033[0m\n";
     }
-    return false;
-}
 
-bool IsUserFound(const std::string& user) {
-    for (auto it = users.begin(); it != users.end(); it++) {
-        if ((*it).second == user) {
-            return true;
-        }
-    }
-    return false;
-}
+    void StartServer () {
+        std::thread th(&Server::AdminPanel, this);
+        th.detach();
 
-void AdminPanel() {
-    while (true) {
-        std::string action = "";
-        std::getline(std::cin, action);
-        std::string command = action.substr(0, action.find(" "));
-        std::string user = action.substr(action.find(" ") + 1, action.size() - 1);
-        if (action.find(" ") == std::string::npos) {
-            std::cout << "\033[91m -- Неправильный формат команды \033[0m\n";
-            continue;
-        }
+        while (true) {
+            sockaddr_in clientAddress{};
+            socklen_t clientAddressLen = sizeof(clientAddress);
 
-        if (!IsCommandFound(command)) {
-            std::cout << "\033[91m -- Команда \"" << command << "\" не найдена. Попробуйте снова: \033[0m";
-            continue;
-        }
-        if (!IsUserFound(user)) {
-            std::cout << "\033[91m -- Пользователь \"" << user << "\" не найден. Попробуйте снова: \033[0m";
-            continue;
-        }
-
-        if (command == "/warn") {
-            std::lock_guard<std::mutex> lock(clientMutex);
-            for (auto it = users.begin(); it != users.end(); it++) {
-                if ((*it).second == user) {
-                    send((*it).first, warning.c_str(), warning.size(), 0);
-                    break;
-                }
+            int clientSocket = accept(serverSocket, (sockaddr*)&clientAddress, &clientAddressLen);
+            if (clientSocket < 0) {
+                std::cerr << "\033[91m -- Ошибка accept\033[0m\n";
+                continue;
             }
-        } else if (command == "/ban") {
-            std::lock_guard<std::mutex> lock(clientMutex);
-            for (auto it = users.begin(); it != users.end(); it++) {
-                if ((*it).second == user) {
-                    send((*it).first, ban.c_str(), ban.size(), 0);
-                    break;
-                }
-            }
+
+            std::thread th(&Server::ClientHandle, this, clientSocket);
+            th.detach();
         }
     }
-}
+
+    ~Server() {
+        close(serverSocket);
+    }
+};
+
 
 int main() {
     std::system("clear");
 
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == -1) {
-        std::cerr << "\033[91m -- Ошибка создания сокета\033[0m\n";
-        return 1;
-    }
+    Server server;
+    server.StartServer();
 
-    sockaddr_in address{};
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY; // слушать все интерфейсы
-    address.sin_port = htons(54000);
-
-    if (bind(serverSocket, (sockaddr*)&address, sizeof(address)) < 0) {
-        std::cerr << "\033[91m -- Ошибка bind\033[0m\n";
-        close(serverSocket);
-        return 1;
-    }
-
-    if (listen(serverSocket, kMaxBacklog) < 0) {
-        std::cerr << "\033[91m -- Ошибка listen\033[0m\n";
-        close(serverSocket);
-        return 1;
-    }
-
-    std::cout << "\033[90m -- Ожидение пользователей...\033[0m\n";
-    std::thread(AdminPanel).detach();
-
-    while (true) {
-        sockaddr_in clientAddress{};
-        socklen_t clientAddressLen = sizeof(clientAddress);
-
-        int clientSocket = accept(serverSocket, (sockaddr*)&clientAddress, &clientAddressLen);
-
-        if (clientSocket < 0) {
-            std::cerr << "\033[91m -- Ошибка accept\033[0m\n";
-            continue;
-        }
-
-        std::thread(ClientHandle, clientSocket).detach();
-    }
-
-    close(serverSocket);
     return 0;
 }
